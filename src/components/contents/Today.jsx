301708ae8fc6d43d32253717bf52c056763f8c35
k_teo2624 <tehokim@snu.ac.kr>
2024-09-02 22:28:30 +0900

    [feat] add today css style

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import { todayText } from "../../data/today";
+
 const Today = () => {
-  return <div>Today</div>;
+  const today = todayText[0];
+
+  return (
+    <div id="today">
+      <div className="today__inner">
+        <div className="today__thumb">
+          <Link to={today.page}>
+            <img src={today.img} alt={today.title} />
+          </Link>
+        </div>
+        <div className="today__text">
+          <p className="today">today!</p>
+          <h3 className="title">
+            <Link to={today.page}>{today.title}</Link>
+          </h3>
+          <p className="desc">{today.desc}</p>
+          <div className="info">
+            <span className="author">
+              <Link to={`/channel/${today.channelId}`}>{today.author}</Link>
+            </span>
+            <span className="date">{today.date}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Today;
