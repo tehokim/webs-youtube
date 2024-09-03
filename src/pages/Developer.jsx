3625b18c95f5681dea0b5ebd42a1877cbdd873b3
k_teo2624 <tehokim@snu.ac.kr>
2024-09-03 17:27:23 +0900

    [feat] add dev page

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+
+import { developerText } from "../data/developer";
+
 import Main from "../components/section/Main";
 
 const Developer = function () {
   return (
     <Main title="추천 개발자" description="오늘의 추천 개발자 유튜버입니다.">
-      Developer
+      <section id="devloperPage">
+        <h2>🥰 오늘의 추천 개발자입니다.</h2>
+        <div className="developer__inner">
+          {developerText.map((dev, key) => (
+            <div className="developer" key={key}>
+              <div className="developer__img play__icon">
+                <Link to={`/channel/${dev.channelId}`}>
+                  <img src={dev.img} alt={dev.name} />
+                </Link>
+              </div>
+              <div className="developer__info">
+                <Link to={`/channel/${dev.channelId}`}>{dev.name}</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   );
 };
