cd131ca34155cf1cb56ab5e41d8d4a64b8569327
k_teo2624 <tehokim@snu.ac.kr>
2024-09-03 17:25:01 +0900

    [feat] add developer component

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
+import { developerText } from "../../data/developer";
+
 const Developer = () => {
-  return <div>Developer</div>;
+  return (
+    <section id="developer">
+      <h2>😪 추천 개발자를 소개합니다.</h2>
+      <div className="developer__inner overflow">
+        {developerText.map((dev, key) => (
+          <div className="developer" key={key}>
+            <div className="developer__img play__icon">
+              <Link to={`/channel/${dev.channelId}`}>
+                <img src={dev.img} alt={dev.name} />
+              </Link>
+            </div>
+            <div className="developer__info">
+              <Link to={`/channel/${dev.channelId}`}>{dev.name}</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Developer;
