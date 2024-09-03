f1030b2203ae85e2e19ff400f604100cd5a5759e
k_teo2624 <tehokim@snu.ac.kr>
2024-09-03 17:38:18 +0900

    [feat] add webd page

--- src/pages/Webd.jsx
+++ src/pages/Webd.jsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
+
 import Main from "../components/section/Main";
+
+import { webdText } from "../data/webd";
 
 const Webd = function () {
   return (
@@ -6,7 +10,20 @@ const Webd = function () {
       title="웹디자인 기능사"
       description="웹디자인 기능사 튜토리얼 강의입니다."
     >
-      Webd
+      <section id="webd">
+        <h2>😮 웹디자인기능사 준비는 이걸로!</h2>
+        <div className="video__inner">
+          {webdText.map((video, key) => (
+            <div className="video" key={key}>
+              <div className="video__thumb play__icon">
+                <Link to={`/video/${video.videoId}`}>
+                  <img src={video.img} alt={video.title} />
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   );
 };
